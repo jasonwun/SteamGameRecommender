8ff9a0fabd0da7d8648abb2be3450b3932ae8f09
Hongyu Ding <hd1004@nyu.edu>
2019-04-19 15:54:27 -0400

    GameListCrawler

---
+++ steamGameListCrawler.py
@@ -0,0 +1,30 @@
+# API website: https://steamapi.xpaw.me/#
+# iterate through game list and store them in json (for now)
+import time
+from steam import *
+import requests
+import json
+
+api = WebAPI(key="D2C78C484D8A9EC812B18BD2E24EE228")
+
+with open ('gamelist.json', 'w') as f:
+    #app list
+    res = requests.get('http://api.steampowered.com/ISteamApps/GetAppList/v2')
+    applist = res.json()['applist']['apps']
+    for items in applist:
+        print(items['appid'])
+        res = requests.get('http://store.steampowered.com/api/appdetails?appids='+str(items['appid']))
+
+        #sleep until request rate is low enough
+        while res.reason == 'Too Many Requests':
+            print('rate limit exceeded')
+            time.sleep(60) #sleep for 60 sec and then request again
+            res = requests.get('http://store.steampowered.com/api/appdetails?appids='+str(items['appid']))
+            
+        resj = res.json()
+        appDetails = resj[str(items['appid'])]
+        print(appDetails['success'])
+        if (appDetails['success'] == False):
+            continue
+        json.dump(appDetails['data'], f, ensure_ascii=False)
+        
