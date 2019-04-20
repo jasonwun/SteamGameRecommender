b3c7ac495046f37646ef30e970559c24a4fdddeb
HongyuDing <hd1004@nyu.edu>
2019-04-19 23:20:07 -0400

    Add logging

--- steamGameListCrawler.py
+++ steamGameListCrawler.py
@@ -3,26 +3,32 @@
 import time
 import requests
 import json
-
+import logging
+log = logging.getLogger('GameListCrawler')
+hdlr = logging.FileHandler('logGameListCrawler.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+log.addHandler(hdlr) 
+log.setLevel(logging.INFO)
 
 with open ('gamelist.json', 'w') as f:
-    #app list
-    res = requests.get('http://api.steampowered.com/ISteamApps/GetAppList/v2')
-    applist = res.json()['applist']['apps']
-    for index,items in enumerate(applist):
-        print(items['appid'])
-        res = requests.get('http://store.steampowered.com/api/appdetails?appids='+str(items['appid']))
-
-        #sleep until request rate is low enough
-        while res.reason == 'Too Many Requests':
-            print('rate limit exceeded')
-            time.sleep(60) #sleep for 60 sec and then request again
+    #with open ('logGameList.log', 'w') as log:
+        #app list
+        res = requests.get('http://api.steampowered.com/ISteamApps/GetAppList/v2')
+        applist = res.json()['applist']['apps']
+        for index,items in enumerate(applist):
             res = requests.get('http://store.steampowered.com/api/appdetails?appids='+str(items['appid']))
+
+            #sleep until request rate is low enough
+            while res.reason == 'Too Many Requests':
+                log.info('rate limit exceeded\n')
+                time.sleep(60) #sleep for 60 sec and then request again
+                res = requests.get('http://store.steampowered.com/api/appdetails?appids='+str(items['appid']))
             
-        resj = res.json()
-        appDetails = resj[str(items['appid'])]
-        print(str(float(index)/float(len(applist))*100) + '  done')
-        if (appDetails['success'] == False):
-            continue
-        json.dump(appDetails['data'], f, indent = 2)
-        
+            resj = res.json()
+            appDetails = resj[str(items['appid'])]
+            log.info("current appid:" + str(items['appid']) + ' Progress:' + str(float(index)/float(len(applist))*100) + '  done\n')
+            if (appDetails['success'] == False):
+                continue
+            json.dump(appDetails['data'], f, indent = 2)
+        log.info('Program ends at ' + str(time.time()) + '\n')
