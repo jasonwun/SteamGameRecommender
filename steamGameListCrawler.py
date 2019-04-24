fca1fde83cdee8e6694eb89ab3dc21740251bbd6
Hongyu Ding <hd1004@nyu.edu>
2019-04-23 21:01:58 -0400

    crawler update output format (remove '[', ']' and ',')

--- steamGameListCrawler.py
+++ steamGameListCrawler.py
@@ -19,7 +19,6 @@ log.setLevel(logging.INFO)
 #last_index = -1
 
 while (True):
-    time.sleep(1)
     #app list
     res = requests.get('http://api.steampowered.com/ISteamApps/GetAppList/v2')
     applist = res.json()['applist']['apps']
@@ -28,8 +27,9 @@ while (True):
     #latest app list is still the same as the last one, no need to re-pull game data
     if (filecmp.cmp('newapplist.json', 'applist.json') == True):
         continue
-    with open ('tmp.json', 'w') as f:
-
+    with open ('applist.json', 'w') as fapps:
+        json.dump(applist, fapps, indent = 2)
+    with open ('completeGameData.json', 'w') as f:
     #    for index, items in enumerate(applist):
     #        if items['appid'] == last_appid:
     #            last_index = index
@@ -38,7 +38,6 @@ while (True):
     #            continue
             try:
                 res = requests.get('http://store.steampowered.com/api/appdetails?appids='+str(items['appid']))
-
                 #sleep until request rate is low enough
                 while res.reason == 'Too Many Requests':
                     log.info('rate limit exceeded\n')
@@ -50,23 +49,26 @@ while (True):
                 log.info("current appid:" + str(items['appid']) + ' Progress:' + str(float(index)/float(len(applist))*100) + '  done\n')
                 if (appDetails['success'] == False):
                     continue
-                json.dump(appDetails['data'], f, indent = 2)
+                json.dump(appDetails['data'], f)
+                f.write('\n')
             except:
                 e = sys.exc_info()[0]
                 log.error("exception found at index: " + str(index) + ", appid: " + str(items['appid']) + ", error: "  + str(e) + "\n")
+    time.sleep(64800) #sleep one day and check applist again
 
     # add '[' at the start of file 
     # add ']' at the end of file 
-    # add ',' to split each json
-    with open("tmp.json", 'r+') as f:
-        with open("completeGameData.json", 'w+') as output:
-            output.write('[')
-            lines = f.readlines()
-            for i in range (0, len(lines)):
-                if (lines[i] == '}{\n'):
-                    lines[i] = '},{\n'
-                lines[i] = lines[i].replace('\n', ' ')
-                output.write(lines[i])
-            output.write(']')
+    
+    # with open("tmp.json", 'r+') as f:
+    #     with open("completeGameData.json", 'w+') as output:
+    #         output.write('[')
+    #         lines = f.readlines()
+    #         for i in range (0, len(lines)):
+    #             # if (lines[i] == '}{\n'):
+    #             #     lines[i] = '},{\n'
+    #             # else:
+    #             #     lines[i] = lines[i].replace('\n', ' ')
+    #             output.write(lines[i])
+    #         output.write(']')
 
-    os.remove("tmp.json")
+    # os.remove("tmp.json")
