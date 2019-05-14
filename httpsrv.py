7147fc1602c6bd92c8c6a2d6f5baf71410ee0d56
JUNDA YIN <yinjunda.cool@gmail.com>
2019-05-14 17:03:09 -0400

    server

--- httpsrv.py
+++ httpsrv.py
@@ -12,6 +12,12 @@ import sys
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from multiprocessing import Process, Pool
+from steam import *
+from time import sleep
+import subprocess
+import json
+
+api = WebAPI(key="D2C78C484D8A9EC812B18BD2E24EE228")
 
 class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
     pass
@@ -24,24 +30,54 @@ class CustomHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        self.wfile.write(("<html><body><h1>hi!</h1></body></html>").encode('utf-8'))
+        self.wfile.write(("<h1>Please enter your UserName on Steam</h1>\
+            <form action=\"Result.html\" method=\"POST\">\
+            <input type=\"text\" name=\"UserName\" id=\"userName\"/>\
+            <input type=\"submit\" value=\"Submit\"/>\
+            </form>").encode('utf-8'))
 
+    
     def do_HEAD(self):
         self._set_headers()
         
     def do_POST(self):
+        html_raw = "<h2>Here's your recommendation provided by leading edge artificial intelligence</h1>"
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        output = sparkTODO(post_data)
+        post_data = self.rfile.read(content_length).decode("utf-8") # <--- Gets the data itself
+        
+        
+        userName = post_data[post_data.index("=")+1:]
+        html_raw  = html_raw + "<h3>" + userName + "</h3><table><tr>\
+        <th>Name</th>\
+        <th>HeaderImage</th>\
+        <th>SteamUrl</th>\
+        </tr>"
+        res = api.call('ISteamUser.ResolveVanityURL', vanityurl=userName)
+        print(type(res))
+
+        print(res["response"]["steamid"])
+        steamID = res["response"]["steamid"]
+        gameResult = api.call('IPlayerService.GetOwnedGames', steamid=steamID, include_appinfo=False, include_played_free_games=True, appids_filter=0)["response"]["games"]
+        sleep(5)
+        print(gameResult)
+
+        gameIds = [i["appid"] for i in gameResult]
+        print(gameIds)
+        head = 5
+        for i in gameIds:
+            cat = subprocess.Popen(["hadoop", "fs", "-cat", "FinalProject/Recommendation/" + i + "/* | -head " + str(head)], stdout=subprocess.PIPE)
+            for line in cat:
+                dump = json.dumps(line)
+                html_raw = html_raw + "<tr><td>" + dump["name"] + "</td>" + "<td><img width=\"200\" height=\"200\" src=\"" + dump["header_image"] + "\"></td>" + "<td><a href=\"https://store.steampowered.com/app/" + dump["steam_appid"] + "\">Visit Steam</a></td>"
         self._set_headers()
-        self.wfile.write(("<html><body><h1>POST!</h1></body></html>" + str(output)).encode('utf-8'))
+        #with open('Result.html', 'r') as content_file:
+        #    content = content_file.read().encode('utf-8')
+        self.wfile.write(html_raw.encode('utf-8'))
     
 
 
 def sparkTODO(steamid):
     return str(steamid) + "sparkoutput"
-
-
 
 
 if sys.argv[1:]:
